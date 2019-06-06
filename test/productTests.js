02aeb6f511cb7103bde538c8eba66f47234b5390
Gboyega Dada <boyega@gmail.com>
2019-06-06 07:12:15 +0100

    Add tests for /products/:id/.../locations, /details and /reviews

--- test/productTests.js
+++ test/productTests.js
@@ -162,4 +162,117 @@ describe(`Test /products routes:`, function() {
 
   });
 
+  
+  describe('3. Fetch product details for product :id', function(done) {
+    let product_id = 45;
+
+    it('returns 200 "OK" with single record', function(done) {
+      request(app).get(`/products/${product_id}/details`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(res => {
+          expect(res.body.product_id).to.be.equal(product_id);
+          expect(res.body).to.have.property('name');
+          expect(res.body).to.have.property('description');
+          expect(res.body).to.have.property('price');
+
+          done();
+        }, done);
+    });
+
+  });
+
+  describe('4. Fetch product locations for product :id', function(done) {
+    let product_id = 45;
+
+    it('returns 200 "OK" with single record', function(done) {
+      request(app).get(`/products/${product_id}/locations`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.have.property('category_id');
+          expect(res.body).to.have.property('category_name');
+          expect(res.body).to.have.property('department_id');
+          expect(res.body).to.have.property('department_name');
+
+          done();
+        }, done);
+    });
+
+  });
+
+  describe('5. Create product reviews for product :id', function(done) {
+    let product_id = 45;
+    let review = {
+      "review": "Hey Jimmy man!!!",
+      "rating": 10
+    };
+    
+    // create a JWT
+    let token = jwt.sign({ id: 1 }, process.env.AUTH0_SECRET, {
+      expiresIn: '10s',
+      audience: process.env.AUTH0_ID
+    });
+
+    it('returns 200 "OK" when USER-KEY is valid', function(done) {
+      request(app).post(`/products/${product_id}/reviews`)
+        .set('Accept', 'application/json')
+        .set('USER-KEY', `Bearer ${token}`)
+        .send(review)
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+
+    it('returns 401 "Unauthorized" when USER-KEY is invalid', function(done) {
+      request(app).post(`/products/${product_id}/reviews`)
+        .set('Accept', 'application/json')
+        .set('USER-KEY', `Bearer ${token}k`)
+        .send(review)
+        .expect('Content-Type', /json/)
+        .expect(401, done);
+    });
+
+    it('returns 400 "Bad Request" when product_id does not exist', function(done) {
+      request(app).post(`/products/1000/reviews`)
+        .set('Accept', 'application/json')
+        .set('USER-KEY', `Bearer ${token}`)
+        .send(review)
+        .expect('Content-Type', /json/)
+        .expect(400, done);
+    });
+
+    it('returns 400 "Bad Request" when rating is invalid', function(done) {
+      request(app).post(`/products/${product_id}/reviews`)
+        .set('Accept', 'application/json')
+        .set('USER-KEY', `Bearer ${token}`)
+        .send({
+          "review": "Hey Jimmy man!!!",
+          "rating": 1000
+        })
+        .expect('Content-Type', /json/)
+        .expect(400, done);
+    });
+  });
+  
+  describe('6. Fetch product reviews for product :id', function(done) {
+    let product_id = 45;
+
+    it('returns 200 "OK" with multiple records', function(done) {
+      request(app).get(`/products/${product_id}/reviews`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(res => {
+          expect(res.body.length).to.be.at.least(1);
+          expect(res.body[0]).to.have.property('review');
+          expect(res.body[0]).to.have.property('rating');
+
+          done();
+        }, done);
+    });
+
+  });
+
 });
