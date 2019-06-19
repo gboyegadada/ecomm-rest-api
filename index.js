fe04e618b5899b55eea0a34812ebef50d7b01f36
Gboyega Dada <boyega@gmail.com>
2019-06-19 22:19:36 +0100

    Add default openshift ip and port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let app = require('./server');
 
-const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.NODE_PORT;
-const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.NODE_IP;
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.NODE_PORT || 8080;
+const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.NODE_IP || '0.0.0.0';
 
 // start server
 app.listen(port, () => {
